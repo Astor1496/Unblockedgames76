38bd48ce0b7f4788401578f231335220837ca2a3
Alexar <166643799+Astor1496@users.noreply.github.com>
2024-09-27 16:27:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+function superMario64() {
+  // Open a new tab
+  var win = window.open();
+  
+  // Add styles to remove margins and padding in the new tab
+  win.document.documentElement.style.margin = "0";
+  win.document.documentElement.style.padding = "0";
+  win.document.documentElement.style.height = "100%";
+  win.document.body.style.margin = "0";
+  win.document.body.style.padding = "0";
+  win.document.body.style.height = "100%";
+  
+  // Create the iframe
+  var url = "https://1v1.lol/";
+  var iframe = win.document.createElement('iframe');
+  iframe.style.position = "absolute";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url;
+  
+  // Focus the iframe once it loads
+  iframe.onload = function() {
+    iframe.focus();
+  };
+  
+  // Append the iframe to the new tab's body
+  win.document.body.appendChild(iframe);
+}
+function superMario64() {
+  // Open a new tab
+  var win = window.open();
+  
+  // Add styles to remove margins and padding in the new tab
+  win.document.documentElement.style.margin = "0";
+  win.document.documentElement.style.padding = "0";
+  win.document.documentElement.style.height = "100%";
+  win.document.body.style.margin = "0";
+  win.document.body.style.padding = "0";
+  win.document.body.style.height = "100%";
+  
+  // Create the iframe
+  var url = "https://mathadventure1.github.io/sm64/sm64/index.html";
+  var iframe = win.document.createElement('iframe');
+  iframe.style.position = "absolute";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url;
+  
+  // Focus the iframe once it loads
+  iframe.onload = function() {
+    iframe.focus();
+  };
+  
+  // Append the iframe to the new tab's body
+  win.document.body.appendChild(iframe);
+}
+
+
+function superMarioBros() {
+  // Open a new tab
+  var win = window.open();
+  
+  // Add styles to remove margins and padding in the new tab
+  win.document.documentElement.style.margin = "0";
+  win.document.documentElement.style.padding = "0";
+  win.document.documentElement.style.height = "100%";
+  win.document.body.style.margin = "0";
+  win.document.body.style.padding = "0";
+  win.document.body.style.height = "100%";
+  
+  // Create the iframe
+  var url = "https://jcw87.github.io/c2-smb1/";
+  var iframe = win.document.createElement('iframe');
+  iframe.style.position = "absolute";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url;
+  
+  // Focus the iframe once it loads
+  iframe.onload = function() {
+    iframe.focus();
+  };
+  
+  // Append the iframe to the new tab's body
+  win.document.body.appendChild(iframe);
+}
+
