346900fe3b23280731b06bbc44b71b14711e35ee
Alexander Runebjer <alexanderrunebjer@gmail.com>
2024-11-13 19:58:35 +0100

    Vex games and drive mad

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ function doPost(e) {
   ).setMimeType(ContentService.MimeType.JSON);
 }
 
+const input = document.querySelector('input');
+const games = document.querySelectorAll('#games img');
+
+input.addEventListener('input', () => {
+  const searchTerm = input.value.toLowerCase();
+  games.forEach(game => {
+    if (game.alt.toLowerCase().includes(searchTerm)) {
+      game.style.display = 'block';
+    } else {
+      game.style.display = 'none';
+    }
+  });
+});
 
 function openGame(url) {
   // Open a new tab
